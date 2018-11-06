ddfe6d024085a98fac71f889dc006fe5c91f584e
YerassylAitkazy <yerassyl.ad@gmail.com>
2018-11-06 23:15:54 +0600

    which triangle 2.0

--- which_triangle/index.rb
+++ which_triangle/index.rb
@@ -1,19 +1,28 @@
-puts 'Введите три стороны треугольника'
- 
-a, b, c = gets.chomp.split(" ").map(&:to_i).sort
+# An empty array of sides of triangle
+arr = []
+
+puts "Введите сторону 'a' треугольника"
+arr.push(gets.chomp.to_f) 
+
+puts "Введите сторону 'b' треугольника"
+arr.push(gets.chomp.to_f)
+
+puts "Введите сторону 'c' треугольника"
+arr.push(gets.chomp.to_f)
+
+arr.sort!
 
 message = ''
 
-if (a + b > c && a + c > b && b + c > a) then
-    if (a == b) then 
+if (arr[0] + arr[1] > arr[2] && arr[1] + arr[2] > arr[0] && arr[0] + arr[2] > arr[1])
+    if (arr[0] == arr[1]) 
         message += "равнобедренный "
-        message += 'равносторенный ' if (a == c)
+        message += 'равносторенный ' if arr[0] == arr[2]
     end
-    message += "прямоугольный " if (c**2 == a**2 + b**2)
+    message += "прямоугольный " if arr[2]**2 == arr[0]**2 + arr[1]**2
     message += "треугольник"
 else
     message += "НЕ существующий треугольник"
 end
 
 puts message
-
